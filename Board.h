294bd6cb267be3ce2fdce429b018b4aea3ff3f7a
Brendan McGrath <bmcgrath2013@gmail.com>
2017-07-23 13:11:44 +0100

    Implemented checkIfWinner function
    Implemented checkIfWinner function to decide if the game has been won by a specific player.
    Added .gitignore file to ignore *.o files.
    Removed trailing whitespace from Board.h file.

--- Board.h
+++ Board.h
@@ -1,60 +1,129 @@
-/**                                                                                          
- * Class that represents the current state of the board.                                     
- */                                                                                          
-class Board{                                                                                 
-                                                                                             
-public:                                                                                      
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   int* getState(){                                                                          
-      return state;                                                                          
-   }                                                                                         
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   void setState( int stateP[] ){                                                            
-      //state = stateP;                                                                      
-   }                                                                                         
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   int getNumFreeSquares(){                                                                  
-      return numFreeSquares;                                                                 
-   }                                                                                         
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   void setSquare( int num, int pos ){                                                       
-      state[ pos ] = num;                                                                    
-      numFreeSquares--;                                                                      
-   }                                                                                         
-                                                                                             
-private:                                                                                     
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   int boardLength = 3;                                                                      
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   int boardHeight = 3;                                                                      
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   int numFreeSquares = 9;                                                                   
-                                                                                             
-   /**                                                                                       
-    *                                                                                        
-    */                                                                                       
-   // change to loop and depends on board size:                                              
-   int state[ 9 ] = {};                                                                      
-                                                                                             
+/**
+ * Class that represents the current state of the board.
+ */
+class Board{
+
+public:
+
+   /**
+    *
+    *
+    * param boardLength:
+    * param boardHeight:
+    */
+   Board( int boardLength, int boardHeight ){
+      setBoardLength( boardLength );
+      setBoardHeight( boardHeight );
+      setNumberFreeSquare( boardLength * boardHeight );
+   }
+
+   /**
+    * Default constructor.
+    */
+   Board(){
+   }
+
+   /**
+    *
+    */
+   int* getState(){
+      return state;
+   }
+
+   /**
+    *
+    */
+   void setState( int stateP[] ){
+      //state = stateP;
+   }
+
+   /**
+    *
+    */
+   int getNumFreeSquares(){
+      return numFreeSquares;
+   }
+
+   /**
+    * Places the players move on the board.
+    *
+    * param num:  The number associated with the player.
+    * param pos:  The position on the board to place the move.
+    *
+    * returns:    True if the move was placed successfully, false otherwise.
+    */
+   bool setSquare( int num, int pos ){
+
+      int* board = getState();
+
+      if( board[ pos ] == 0 ){
+         // change to setState/Pos
+         state[ pos ] = num;
+         numFreeSquares--;
+         return true;
+      }
+
+   return false;
+
+   }
+
+   /**
+    *
+    */
+   void setBoardLength( int boardLengthP ){
+      boardLength = boardLengthP;
+   }
+
+   /**
+    *
+    */
+   int getBoardLength(){
+      return boardLength;
+   }
+
+   /**
+    *
+    */
+   void setBoardHeight( int boardHeightP ){
+      boardHeight = boardHeightP;
+   }
+
+   /**
+    *
+    */
+   int getBoardHeight(){
+      return boardHeight;
+   }
+
+   /**
+    *
+    */
+   void setNumberFreeSquare( int numFreeSquaresP ){
+      numFreeSquares = numFreeSquaresP;
+   }
+
+
+
+private:
+
+   /**
+    *
+    */
+   int boardLength;
+
+   /**
+    *
+    */
+   int boardHeight;
+
+   /**
+    * Set with constructor?
+    */
+   int numFreeSquares;
+
+   /**
+    * Dynamically set with constructor, and remove with destructor?
+    */
+   int state[ 9 ] = {};
+
 }; 
--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 // todo:
-// -algorithm to check if anyone has won (checkIfWinner).
+// -checkIfWinner should work for any number of dimensions and any board length, size, etc.
 // -check if square taken before setting. just don't set (no error message). reissue pick a
 //    number message.
 // -drawBoard once more before stating Draw.
-
+// -check is user input numeric, number in range, and check if square not already taken.
+//    only reduce num free squares if valid input.
+// -need a Game class with Board object as member?
 
 /**
  * An XO (tic-tac-toe) game implementation.
  */
 #include <iostream>
-
 #include "Board.h"
-
-void drawBoard();
-bool checkIfWinner( Board b );
 
 
 /**
@@ -21,6 +19,82 @@ bool checkIfWinner( Board b );
  *
  * param b: The board object.
  */
+void drawBoard( Board b );
+
+/**
+ * Checks to see if the current player has won.
+ *
+ * param b: The board object for the current game.
+ *
+ * returns: True if the player has won, false otherwise.
+ */
+bool checkIfWinner( Board b );
+
+
+int main(){
+
+   std::cout << "Hello" << std::endl;
+
+   bool winner = false;
+
+   // Create a new instance of the game board:
+   Board board( 3, 3 );
+
+   //board.setSquare( 1, 3 );
+   //board.setSquare( 2, 4 );
+
+   int gameIteration = 0;
+   int player = 1;
+   int playerInput = 0;
+
+   // Main game loop:
+   while( ( not winner ) and ( 0 != board.getNumFreeSquares() ) ){
+
+      // Draw the board on standard output:
+      drawBoard( board );
+
+      // Decide who is playing in this round:
+      if( 0 == gameIteration % 2 )
+         player = 1;
+      else
+         player = 2;
+
+      // how can the user identify the square?
+      //    pick a row and a column instead?
+      //    use a 2d int array?
+
+      bool movePlaced = false;
+
+      while( not movePlaced ){
+         std::cout << "Player " << player << ", please enter a square:" << std::endl;
+         std::cin >> playerInput;
+
+         if( board.setSquare( player, playerInput ) )
+            movePlaced = true;
+      }
+
+      // check if anyone has won.
+      if( checkIfWinner( board ) )
+         winner = true;
+      //else if( 0 = board.numFreeSquares )
+         //break;
+
+      gameIteration++;
+
+   }// End of main game loop.
+
+
+   if( winner ){
+      drawBoard( board );
+      std::cout << "Player " << player << " has won" << std::endl;
+   }else{
+      drawBoard( board );
+      std::cout << "Draw" << std::endl;
+   }
+
+
+}
+
 void drawBoard( Board b ){
 
    int* boardState = b.getState();
@@ -54,75 +128,34 @@ void drawBoard( Board b ){
 
 }
 
-int main(){
-
-   std::cout << "Hello" << std::endl;
-
-   bool winner = false;
-
-   // Create a new instance of the game board:
-   Board board;
-
-   //board.setSquare( 1, 3 );
-   //board.setSquare( 2, 4 );
-
-   int gameIteration = 0;
-   int player = 1;
-   int playerInput = 0;
-
-   // Main game loop:
-   while( ( not winner ) and ( 0 != board.getNumFreeSquares() ) ){
-
-      // Draw the board on standard output:
-      drawBoard( board );
-
-      // Decide who is playing in this round:
-      if( 0 == gameIteration % 2 )
-         player = 1;
-      else
-         player = 2;
-
-      // how can the user identify the square?
-      //    pick a row and a column instead?
-      //    use a 2d int array?
-
-      // check is input numeric, number in range, and check if square not already taken.
-
-      std::cout << "Player " << player << ", please enter a square:" << std::endl;
-      std::cin >> playerInput;
-
-      board.setSquare( player, playerInput );
-
-      // check if anyone has won.
-      if( checkIfWinner( board ) )
-         winner = true;
-      //else if( 0 = board.numFreeSquares )
-         //break;
-
-      gameIteration++;
-
-   }// End of main game loop.
-
-
-   if( winner )
-      std::cout << "Player " << player << " has won" << std::endl;
-   else
-      std::cout << "Draw" << std::endl;
-
-
-}
-
-/**
- * Checks to see if the current player has won.
- *
- * param b: The board object for the current game.
- *
- * returns: True if the player has won, false otherwise.
- */
+// generalise to work with n dimensions.
 bool checkIfWinner( Board b ){
 
-   bool winner = false;
+   int bLength = b.getBoardLength();
+   int bHeight = b.getBoardHeight();
+
+   // Get the state of the board:
+   int* bS = b.getState();
+
+   // check verticals:
+   // extend to general board length and size:
+   for( int i = 0; i < bLength; i++ )
+      if( ( bS[ i ] == bS[ i + 3 ] ) and ( bS[ i ] == bS[ i + 6 ] ) and ( bS[ i ] != 0 ) )
+         return true;
+
+   // check horizontals:
+   // extend to general board length and size:
+   for( int i = 0; i < bHeight; i++ )
+      if( ( bS[ 3 * i ] == bS[ 3 * i + 1 ] ) and ( bS[ 3 * i ] == bS[ 3 * i + 2 ] ) and ( bS[ 3 * i ] != 0 ) )
+         return true;
+
+   // check verticals:
+   // extend to general board length and size:
+   if( ( bS[ 0 ] == bS[ 4 ] ) and ( bS[ 0 ] == bS[ 8 ] ) and bS[ 0 ] != 0 )
+      return true;
+   if( ( bS[ 2 ] == bS[ 4 ] ) and ( bS[ 2 ] == bS[ 6 ] ) and bS[ 0 ] != 0 )
+      return true;
    
-   return winner;
+   return false;
 
 }
